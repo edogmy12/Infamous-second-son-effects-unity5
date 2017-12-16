33f507d74a66ed4ae832a3cd73fddafd09b63cae
edogmy12 <34609848+edogmy12@users.noreply.github.com>
2017-12-16 15:05:59 -0800

    Bomb added
    Bomb added still working on it though

---
+++ Infamous Game/Assets/BombExplode.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombExplode : MonoBehaviour
+{
+    public float delay = 3f;
+    float countdown;
+    bool hasExploded = false;
+    public float force = 700;
+    public float Radius = 5f;
+
+    public GameObject explosionEffect;
+
+
+    void Start()
+    {
+        countdown = delay;
+    }
+
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown <= 0f && !hasExploded)
+        {
+            Explode();
+            hasExploded = true;
+        }
+    }
+
+    void Explode()
+    {
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+        Destroy(explosionEffect);
+        Collider [] colliders = Physics.OverlapSphere(transform.position, Radius);
+
+        foreach (Collider nearbyObject in colliders)
+        {
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+
+            if (rb != null)
+            {
+                rb.AddExplosionForce(force, transform.position, Radius);
+            }
+        }
+        Destroy(gameObject);
+        
+    }
+}
---
+++ Infamous Game/Assets/DestroyParticleSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyParticleSystem : MonoBehaviour
+{
+private var ps : ParticleSystem;
+ 
+ ////////////////////////////////////////////////////////////////
+ 
+ function Start () {
+          ps = GetComponent(ParticleSystem);
+ }
+ 
+ function Update () {
+     if(ps)
+     {
+         if(!ps.IsAlive())
+         {
+             Destroy(gameObject);
+         }
+     }
+ }
+}
